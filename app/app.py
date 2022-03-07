1dc79b7c41b7ec8af5ef6b890756f686b85a68af
Henry Boisgibault <h.boisgibault@gmail.com>
2022-03-07 10:19:35 +0100

    API Documentation

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
 from flask_caching import Cache
+from flask_cors import CORS
+from flasgger import Swagger
 import os
 from dotenv import load_dotenv
 from config import Config
@@ -18,13 +20,74 @@ db.init_app(app)
 
 migrate = Migrate(app, db)
 
+CORS(app)
+
+app.config['SWAGGER'] = {
+    'title': 'Logora NLP',
+    'description': 'A collection of tools to analyse collections of documents.',
+    'version': '1.0.0',
+    'uiversion': 3,
+    'specs_route': '/docs/'
+}
+swagger = Swagger(app)
+
 @app.route('/')
 def status():
-    return jsonify({"status": "everything is allright"})
+  """Status endpoint.
+    ---
+    tags:
+      - status
+    responses:
+      200:
+        description: A success message
+        schema:
+          type: object
+          properties:
+            status:
+              type: string
+              description: Status message.
+              example: 'everyhing is allright'
+  """
+  return jsonify({"status": "everything is allright"})
 
 # Get analysis
 @app.route('/analysis/<uid>', methods=['GET'])
 def get_analysis(uid):
+  """Get analysis.
+    ---
+    tags:
+      - analysis
+    parameters:
+      - name: uid
+        in: path
+        type: string
+        required: true
+    responses:
+      200:
+        description: Analysis content
+        schema:
+          type: object
+          properties:
+            success:
+              type: boolean
+              description: Success message.
+              example: 'true'
+            data:
+              type: object
+              properties:
+                uid:
+                  type: string
+                  description: Analysis unique identifier
+                  example: '146464-364346'
+                name:
+                  type: string
+                  description: Analysis name
+                  example: 'keyword_extraction'
+                content:
+                  type: string
+                  description: Analysis content in JSON format
+                  example: '{}'
+  """
   analysis = Analysis.query.filter_by(uid=uid).first()
   if analysis is None:
     return make_response(jsonify({"success": False, "error": "Object not found"}), 404)
@@ -34,6 +97,42 @@ def get_analysis(uid):
 # Create analysis
 @app.route('/analysis', methods=['POST'])
 def create_analysis():
+  """Create analysis.
+    ---
+    tags:
+      - analysis
+    parameters:
+      - name: uid
+        in: query
+        type: string
+        required: true
+      - name: name
+        in: query
+        type: string
+        enum: ['test']
+        required: true
+    responses:
+      200:
+        description: Analysis content
+        schema:
+          type: object
+          properties:
+            success:
+              type: boolean
+              description: Success message.
+              example: 'true'
+            data:
+              type: object
+              properties:
+                uid:
+                  type: string
+                  description: Analysis unique identifier
+                  example: '146464-364346'
+                name:
+                  type: string
+                  description: Analysis name
+                  example: 'keyword_extraction'
+  """
   uid = request.args.get('uid')
   name = request.args.get('name')
   if uid and name:
