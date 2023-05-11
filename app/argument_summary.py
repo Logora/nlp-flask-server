35f1cb0a47ee74ebc606b416500bd8d8c3ac9c74
Henry Boisgibault <h.boisgibault@gmail.com>
2023-05-11 10:50:36 +0200

    Argument summary analysis

--- app/argument_summary.py
+++ app/argument_summary.py
@@ -1,10 +1,59 @@
 import pandas as pd
+import json
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.docstore.document import Document
+from langchain import LLMChain, PromptTemplate
+from langchain.llms import OpenAI
+from langchain.chains.summarize import load_summarize_chain
 
 def get_summary(uid, documents, question):
-  return []
+  separator = "\n\n--------------------\n\n"
+  long_text = separator.join(documents)
+
+  text_splitter = CharacterTextSplitter(        
+      separator=separator,
+      chunk_overlap=0
+  )
+
+  texts = text_splitter.split_text(long_text)
+  docs = [Document(page_content=t) for t in texts]
+
+  llm = OpenAI(temperature=0, model_name='text-davinci-003', batch_size=10)
+
+  map_template = """
+    Ta tâche est de résumer des contributions de personnes ayant répondu au débat suivant : {question}.
+    Voici les contributions séparées par une ligne :
+
+    {text}
+    
+    Résume ces contributions en affichant les trois arguments les plus récurrents. Pour chaque argument, donne aussi leur nombre d'occurrences dans les contributions.
+    Ces arguments doivent être de 180 caractères maximum chacun et être classés du plus récurrent au moins récurrent.
+
+    ARGUMENTS:"""
+  map_prompt = PromptTemplate(template=map_template, input_variables=['text', 'question'])
+
+  reduce_template = """
+    Ta tâche est de résumer des arguments en réponse au débat suivant : {question}.
+    Voici les arguments séparées par une ligne :
+
+    {text}
+    
+    Résume ces arguments en affichant les trois arguments les plus récurrents. Pour chaque argument, ajoute aussi leur nombre d'occurrences.
+    Ces arguments doivent être de 180 caractères maximum chacun et être classés du plus récurrent au moins récurrent, et sous format JSON.
+
+    ARGUMENTS:"""
+  reduce_prompt = PromptTemplate(template=reduce_template, input_variables=['text', 'question'])
+
+  chain = load_summarize_chain(llm, chain_type="map_reduce", map_prompt=map_prompt, combine_prompt=reduce_prompt)
+
+  output = chain({"input_documents": docs, "question": question})
+
+  arguments = json.loads(output['output_text'])
+  json_analysis = build_json(arguments)
+  return json_analysis
 
 def build_json(arguments):
   analysis = []
   for i, k in enumerate(arguments):
-    analysis.append({ "id": i, "name": k[0], "weight": k[1] })
+    analysis.append({ "id": i, "argument": k["argument"], "weight": k["occurrences"] })
   return analysis
