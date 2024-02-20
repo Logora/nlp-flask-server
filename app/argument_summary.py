493004599871900a162166be927bcd0420a34451
Mboulgour <mboulgour@protonmail.com>
2024-02-20 15:30:33 +0100

    Test

--- app/argument_summary.py
+++ app/argument_summary.py
@@ -49,6 +49,7 @@ def get_summary(uid, documents, question, language='fr', model_name='gpt-3.5-tur
     
     arguments = []
 
+    print(points)
     for point in points:
         index, rest = point.split(".", 1)
         description, recurrence = rest.split("(Récurrence: ", 1)
@@ -59,6 +60,7 @@ def get_summary(uid, documents, question, language='fr', model_name='gpt-3.5-tur
             "occurrences": weight
         })
 
+    print(arguments)
     json_analysis = build_json(arguments)
     return json_analysis
 
